08f3ca718b67f18e244229fea4458494982aee44
Enrico Reimer <reimer@cbs.mpg.de>
2023-08-09 15:30:01 +0200

    some cleanup, move cli source to src/bin

--- src/bin/cli.rs
+++ src/bin/cli.rs
@@ -1,27 +1,27 @@
-mod db;
-mod dcm;
-mod file;
-mod config;
-
 use anyhow::{Context, Result};
 use std::path::PathBuf;
 
 use clap::Parser;
-use crate::file::import_glob;
+use rudicom::{db,config,file::import_glob};
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
     // file or globbing to open
     filename: PathBuf,
+    // config file
+    config: Option<PathBuf>,
+    // url of the database to connect to
+    #[arg(default_value_t = String::from("ws://localhost:8000"))]
+    database: String,
 }
 
 #[tokio::main]
 async fn main() -> Result<()>
 {
     let args = Cli::parse();
-    config::init(None)?;
-    db::init("ws://localhost:8000").await.context(format!("Failed connecting to ws://localhost:8000"))?;
+    config::init(args.config)?;
+    db::init(args.database.as_str()).await.context(format!("Failed connecting to {}",args.database))?;
 
     let pattern = args.filename.to_str().expect("Invalid string");
     import_glob(pattern).await;
---
+++ src/lib.rs
@@ -0,0 +1,5 @@
+pub mod db;
+pub mod dcm;
+pub mod file;
+pub mod config;
+
