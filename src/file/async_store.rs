b0c1def1ba20212355f67cd9f19599326be66f71
Enrico Reimer <reimer@cbs.mpg.de>
2023-08-12 14:55:31 +0200

    separate filepath generation between local/onwned and complete add option md5 generation for writing too

--- src/file/async_store.rs
+++ src/file/async_store.rs
@@ -6,19 +6,33 @@ use md5::Context;
 use tokio::fs::File;
 use tokio::io::{AsyncWriteExt,AsyncReadExt};
 
+pub fn read<T>(input: T, with_md5:Option<&mut Context>) -> Result<DefaultDicomObject>
+	where T:AsRef<[u8]>
+{
+	let mut buffer = Cursor::new(input);
+	if let Some( md5) = with_md5{
+		std::io::copy(&mut buffer,md5).unwrap();
+	}
+	buffer.seek(SeekFrom::Start(128)).unwrap(); // preamble
+	Ok(from_reader(buffer)?)
+}
 
-fn mem_write(obj:&DefaultDicomObject) -> Result<Cursor<Vec<u8>>>{
-	let mut out = Cursor::new(Vec::<u8>::new());
+pub fn write(obj:&DefaultDicomObject, with_md5:Option<&mut Context>) -> Result<Cursor<Vec<u8>>>{
+	let mut out = Cursor::new(Vec::new());
 	out.seek(SeekFrom::Start(128))?;
 	Write::write_all(&mut out, b"DICM")?;
 	obj.write_meta(&mut out)?;
 	obj.write_dataset(&mut out)?;
+	if let Some( md5) = with_md5{
+		out.seek(SeekFrom::Start(0))?;
+		std::io::copy(&mut out,md5).unwrap();
+	}
 	Ok(out)
 }
 
-pub async fn write_file(path:&PathBuf, obj:&DefaultDicomObject)->Result<()>{
+pub async fn write_file(path:&PathBuf, obj:&DefaultDicomObject,with_md5:Option<&mut Context>)->Result<()>{
 	let mut file = File::create(path).await?;
-	let data= mem_write(obj)?.into_inner();
+	let data= write(obj,with_md5)?.into_inner();
 	file.write_all(data.as_slice()).await?;
 	file.flush().await?;
 	Ok(())
@@ -27,9 +41,6 @@ pub async fn write_file(path:&PathBuf, obj:&DefaultDicomObject)->Result<()>{
 pub async fn read_file(path:PathBuf,with_md5:Option<&mut Context>) -> Result<DefaultDicomObject>{
 	let mut buffer = Vec::<u8>::new();
 	File::open(path).await?.read_to_end(&mut buffer).await?;
-	if let Some( md5) = with_md5{
-		md5.write_all(buffer.as_slice()).unwrap();
-	}
-	buffer.drain(..128); // preamble
-	Ok(from_reader(Cursor::new(buffer))?)
+	read(buffer,with_md5)
 }
+
--- src/tools/store.rs
+++ src/tools/store.rs
@@ -1,16 +1,16 @@
 use std::collections::BTreeMap;
 use anyhow::{Context, Result};
 use dicom::object::DefaultDicomObject;
-use crate::{db, register_instance, RegistryGuard};
-use crate::db::JsonValue;
-use crate::dcm::gen_filepath;
+use crate::{DbVal, register_instance, RegistryGuard};
+use crate::JsonValue;
+use crate::dcm::{complete_filepath, gen_filepath};
 use crate::file::async_store::write_file;
 
 pub async fn store(obj:DefaultDicomObject,checksum:md5::Digest) -> Result<JsonValue>
 {
 	let path = gen_filepath(&obj);
-	let fileinfo:BTreeMap<String,db::DbVal>= BTreeMap::from([
-		("path".into(),path.to_str().unwrap().into()),
+	let fileinfo:BTreeMap<String,DbVal>= BTreeMap::from([
+		("path".into(),path.clone().into()),
 		("owned".into(),true.into()),
 		("md5".into(),format!("{:x}", checksum).into())
 	]);
@@ -18,7 +18,8 @@ pub async fn store(obj:DefaultDicomObject,checksum:md5::Digest) -> Result<JsonVa
 	let mut guard=RegistryGuard::default();
 	let registered = register_instance(&obj,vec![("file".into(),fileinfo.into())],Some(&mut guard)).await?;
 	if registered.is_null() {
-		write_file(&path,&obj).await.context(format!("Failed to write file {}",path.display()))?;
+		let path = complete_filepath(path);
+		write_file(&path,&obj,None).await.context(format!("Failed to write file {}",path.display()))?;
 		guard.reset();//all good, we can drop the guard
 	}
 	Ok(registered)
