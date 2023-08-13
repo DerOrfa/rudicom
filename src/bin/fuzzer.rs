e8e3bc62f0da7dde35102b24d1656d5b8dcefc41
Enrico Reimer <reimer@cbs.mpg.de>
2023-08-13 20:32:40 +0200

    parallelized the fuzzer

--- src/bin/fuzzer.rs
+++ src/bin/fuzzer.rs
@@ -10,6 +10,7 @@ use reqwest::{Body, Client};
 use rudicom::storage::async_store::read_file;
 use rudicom::Result;
 
+static INSTACE_FACTOR:u32=100;
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
@@ -18,9 +19,38 @@ struct Cli {
 	server: String,
 }
 
-fn get_id(obj:&DefaultDicomObject,tag:Tag) -> Result<Vec<String>>
+async fn send_instance(obj:DefaultDicomObject,url:&str) -> Result<()>{
+	let buffer=rudicom::storage::async_store::write(&obj,None)?.into_inner();
+
+	let request = Client::new()
+		.post(url)
+		.body(Body::from(buffer));
+
+	request.send().await?.error_for_status()?;
+	print!(".");
+	io::stdout().flush().unwrap();
+	Ok(())
+}
+
+fn set_tag(obj:&mut DefaultDicomObject,new_val:String,tag:Tag) -> Result<()>
 {
-	Ok(obj.element(tag)?.to_str()?.split('.').map(|s|s.to_string()).collect())
+	let mut e=obj.take_element(tag).unwrap();
+	let mut vals:Vec<_>=e.to_str()?.split('.').map(|s|s.to_string()).collect();
+	*vals.last_mut().unwrap()=new_val;
+	e.update_value(|v|
+		*v.primitive_mut().unwrap()=PrimitiveValue::from(vals.join("."))
+	);
+	obj.put_element(e);
+	Ok(())
+}
+
+async fn modify_and_send(mut copy:DefaultDicomObject,instance:u32, series:u32, study:u32, url:String) -> Result<()>
+{
+	set_tag(&mut copy, (study*10*INSTACE_FACTOR+series*INSTACE_FACTOR+instance).to_string(), tags::SOP_INSTANCE_UID)?;
+	set_tag(&mut copy, (study*10+series).to_string(), tags::SERIES_INSTANCE_UID)?;
+	set_tag(&mut copy, study.to_string(), tags::STUDY_INSTANCE_UID)?;
+
+	send_instance(copy, url.as_str()).await
 }
 
 #[tokio::main]
@@ -28,31 +58,21 @@ async fn main() -> Result<()>
 {
 	let path=PathBuf::from("assets/MR000000.IMA");
 	let artifact=read_file(path,None).await?;
-	let mut instance_id = get_id(&artifact,tags::SOP_INSTANCE_UID)?;
-	let series_id = get_id(&artifact,tags::SERIES_INSTANCE_UID)?;
-	let study_id = get_id(&artifact, tags::STUDY_INSTANCE_UID)?;
+	let mut tasks=tokio::task::JoinSet::new();
 
 	let args = Cli::parse();
-	let client=Client::new();
-
-	for i in 0..1000
-	{
-		*instance_id.last_mut().unwrap()=i.to_string();
-		let mut copy=artifact.clone();
-		let mut e=copy.take_element(tags::SOP_INSTANCE_UID).unwrap();
-		e.update_value(|v|
-				*v.primitive_mut().unwrap()=PrimitiveValue::from(instance_id.join("."))
-		);
-		copy.put_element(e);
-		let buffer=rudicom::storage::async_store::write(&copy,None)?.into_inner();
-
-		let request = client
-			.post(args.server.clone()+"/instances")
-			.body(Body::from(buffer));
-
-		request.send().await?.error_for_status()?;
-		print!(".");
-		io::stdout().flush().unwrap();
+	let url = args.server.clone() + "/instances";
+	for study in 0..10 {
+		for series in 0..10 {
+			for instance in 0..INSTACE_FACTOR
+			{
+				tasks.spawn(modify_and_send(artifact.clone(),instance,series,study,url.clone()));
+				while tasks.len() > 10 {
+					tasks.join_next().await.unwrap()??;
+				}
+			}
+		}
 	}
-	todo!()
+	while let Some(t) = tasks.join_next().await {}
+	Ok(())
 }
