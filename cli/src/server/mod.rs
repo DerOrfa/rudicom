3184815a0d0c3018e7aa9e7c19cab226c80cf2cd
Enrico Reimer <reimer@cbs.mpg.de>
2024-01-28 02:28:19 +0100

    add handler for graceful shutdown

--- cli/src/server/mod.rs
+++ cli/src/server/mod.rs
@@ -3,6 +3,7 @@ use axum::extract::DefaultBodyLimit;
 use axum::routing::get;
 use serde::Serialize;
 use tokio::net::TcpListener;
+use tokio::signal;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 use tracing;
 use crate::{config, db};
@@ -62,5 +63,31 @@ pub async fn serve(listener:TcpListener) -> Result<()>
 	}
 
 	// run it
-	axum::serve(listener,app.into_make_service()).await.map_err(|e|e.into())
+	axum::serve(listener,app.into_make_service())
+		.with_graceful_shutdown(shutdown_signal())
+		.await.map_err(|e|e.into())
+}
+
+async fn shutdown_signal() {
+	let ctrl_c = async {
+		signal::ctrl_c()
+			.await
+			.expect("failed to install Ctrl+C handler");
+	};
+
+	#[cfg(unix)]
+		let terminate = async {
+		signal::unix::signal(signal::unix::SignalKind::terminate())
+			.expect("failed to install signal handler")
+			.recv()
+			.await;
+	};
+
+	#[cfg(not(unix))]
+		let terminate = std::future::pending::<()>();
+
+	tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
+    }
 }
